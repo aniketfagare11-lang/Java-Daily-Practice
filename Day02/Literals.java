ab380dd8cfbe03d7ae780438979eb2aaa5d208db
Aniketfagare11-lang <aniketfagare11@gmail.com>
2026-01-17 00:22:23 +0530

    Day 02 - Variables & Literals

---
+++ Day02/Literals.java
@@ -0,0 +1,14 @@
+public class Literals {
+    public static void  main (String[] args)
+    {
+        byte age=36;
+        int age2= 34;
+        short age3=33;
+        long ageDino=5666839l;
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(age3);
+        
+
+    }
+}
