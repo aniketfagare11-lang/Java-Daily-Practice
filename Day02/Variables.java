872cc96bc58d53d38d556d5ab6b4b4b76be3d374
Aniketfagare11-lang <aniketfagare11@gmail.com>
2026-01-17 00:09:38 +0530

    Day02 -- variables in Java

---
+++ Day02/Variables.java
@@ -0,0 +1,11 @@
+
+public class Variables {
+    
+    public static void main (String [] args ){
+        int a=9,b=8,c=7; // a,b,c -- are the variables
+        int sum =a+b+c;
+        System.out.print("The sum of Three Numbers :");
+        System.out.println(sum);
+
+    }
+}
