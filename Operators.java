ca5d1f866f9aa1c6f9e8f0968ecf4453f72bfa39
Aniketfagare11-lang <aniketfagare11@gmail.com>
2026-01-18 00:17:26 +0530

    Day 03 - Operators in Java

---
+++ Operators.java
@@ -0,0 +1,28 @@
+public class Operators {
+ 
+    public static void main (String[] args)
+    {
+        int a =9;
+        int b =10;
+        int c=a+b; // arithmatic operation & = is Assignment Operation
+        System.out.println(c);
+        System.out.println(a<=b); // Comparison Operation
+        System.out.println(a>b && a<b); //Logical Operation
+        System.out.println(a^b); // Bitwise Operation
+        a++; // Increment Operation
+        System.out.println(a);
+        a--; // Decrement Operation
+        System.out.println(a);
+
+        // In This code i'm only takes the one example each for operators..
+
+        //Ouick Quiz Q-- Find the value of x..
+        int y = 7;
+        int x = ++y*8;
+        System.out.println(x);
+
+    }
+    
+}
+
+
