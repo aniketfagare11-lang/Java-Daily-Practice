89ab3b8f2a3d6b9326f6c4ee845a71969280a470
Aniketfagare11-lang <aniketfagare11@gmail.com>
2026-01-20 00:12:52 +0530

    Day 05 - If-Else in Java

---
+++ Day05/if_Else.java
@@ -0,0 +1,23 @@
+package Day05;
+
+public class if_Else {
+
+    public static void main (String [] args)
+    {
+        int age = 26;
+        if(age >= 18)
+            {
+                System.out.println("You can Drive a Car!! ");
+            } 
+        else 
+            {
+            System.out.println("You cannot Drive a Car!! ");
+            }
+    }
+}
+
+// Relational Operations in Java:
+// (==,!=,>=,<=,<,>) These are the Operation we mostly use in conditional Statements
+
+    
+
