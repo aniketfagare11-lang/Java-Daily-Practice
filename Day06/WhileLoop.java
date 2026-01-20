c39e13cc2891ff6830f64eb1b99c8ab567563fdc
Aniketfagare11-lang <aniketfagare11@gmail.com>
2026-01-21 00:06:53 +0530

    Day 06 - While Loop In Java

---
+++ Day06/WhileLoop.java
@@ -0,0 +1,27 @@
+package Day06;
+
+public class WhileLoop {
+    public static void main (String [] args)
+    {
+        int i=1;
+        while(i<=3) //While Loop in Java...
+        {
+            System.out.println(i);
+            i++;
+        }
+
+        // Question -- Print 100 to 200 Natural Numbers..
+        int a=100;
+        System.out.println("Printing 100 to 200 Natural Numbers:");
+        while(a<=200) 
+        {
+            System.out.println(a);
+            a++;
+        }   
+                   
+    }
+    
+}
+
+    
+
